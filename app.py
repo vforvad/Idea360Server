9325d6b735db0eedfca115aecfd27247f2e0af28
Vadim Sokoltsov <vforvad@gmail.com>
2017-09-17 00:51:22 +0300

    add database and migration support with specific config

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 from flask import Flask
 from flask_restful import Resource, Api
+from flask.ext.sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
 api = Api(app)
+db = SQLAlchemy(app)
+
+from models import *
 
 class HelllWorld(Resource):
     def get(self):
