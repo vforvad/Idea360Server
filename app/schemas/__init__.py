2ac9380f2be640c41fc95885c88d2a6b84681838
Vadim Sokoltsov <vforvad@gmail.com>
2017-10-16 01:47:10 +0300

    change simple users_schema

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,6 +1,4 @@
 from flask_marshmallow import Marshmallow
 ma = Marshmallow()
 
-from .user import UserSchema
-user_schema = UserSchema()
-users_schema = UserSchema(many=True)
+from .user import UserSchema, user_schema, users_schema
