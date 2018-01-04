63b95950f5fdd286c2933af7d77a91b6a773b617
Vadim Sokoltsov <vforvad@gmail.com>
2018-01-05 01:05:28 +0300

    add company form-object

--- app/forms/__init__.py
+++ app/forms/__init__.py
@@ -1,7 +1,7 @@
 from wtforms import Form, fields, validators
 import sqlalchemy
 
-from app.models import db, User
+from app.models import db, User, Company
 from app.services import encode_user
 
 from .registration import RegistrationForm
---
+++ app/forms/company.py
@@ -0,0 +1,29 @@
+from . import (
+    Form, fields, validators, db,
+    Company, sqlalchemy
+)
+
+class CompanyForm(Form):
+    """ Form object for creating the company """
+
+    name = fields.StringField('name', [validators.DataRequired()])
+    description = fields.StringField('description')
+    start_date = fields.DateField('start_date', format='%d-%m-%Y')
+    city = fields.StringField('city')
+
+    def submit(self):
+        """ Create new company instance """
+
+        if not self.validate(): return False
+
+        params = {
+            'name': self.name.data,
+            'description': self.description.data,
+            'start_date': self.start_date.data,
+            'city': self.city.data
+        }
+        company = Company(**params)
+        db.session.add(company)
+        db.session.commit()
+        self.object = company
+        return True
